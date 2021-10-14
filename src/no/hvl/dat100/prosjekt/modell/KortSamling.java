1412b604d92d3048fdc570eebf8f799535cc24d0
haukh <haukh@DESKTOP-C8GN5EU.home>
2021-10-14 14:18:09 +0200

    Kortsamling nesten ferdig (leggTilAlle assertionError)

--- src/no/hvl/dat100/prosjekt/modell/KortSamling.java
+++ src/no/hvl/dat100/prosjekt/modell/KortSamling.java
@@ -1,16 +1,16 @@
 package no.hvl.dat100.prosjekt.modell;
 
-
 import no.hvl.dat100.prosjekt.TODO;
 import no.hvl.dat100.prosjekt.kontroll.dommer.Regler;
 
 /**
  * Struktur for å lagre ei samling kort. Kan lagre hele kortstokken. Det finnes
- * en konstant i klassen Regler som angir antall kort i hver av de 4 fargene. Når
- * programmet er ferdig settes denne til 13, men under utvikling / testing kan
- * det være praktisk å ha denne mindre.
+ * en konstant i klassen Regler som angir antall kort i hver av de 4 fargene.
+ * Når programmet er ferdig settes denne til 13, men under utvikling / testing
+ * kan det være praktisk å ha denne mindre.
  * 
  */
+
 public class KortSamling {
 
 	private final int MAKS_KORT = 4 * Regler.MAKS_KORT_FARGE;
@@ -22,82 +22,77 @@ public class KortSamling {
 	 * Oppretter en tom Kortsamling med plass til MAKS_KORT (hele kortstokken).
 	 */
 	public KortSamling() {
-		
-Kort[] samling = new Kort[MAKS_KORT];
+
+		samling = new Kort[MAKS_KORT];
+		antall = 0;
+
 	}
 
 	/**
 	 * Returnerer en tabell med kortene i samlinga. Tabellen trenger ikke være
-	 * full. Kortene ligger sammenhengende fra starten av tabellen. Kan få
-	 * tilgang til antallet ved å bruke metoden getAntallKort(). Metoden er
-	 * først og fremst ment å brukes i testklasser. Om man trenger
-	 * kortene utenfor, anbefales metoden getAlleKort().
+	 * full. Kortene ligger sammenhengende fra starten av tabellen. Kan få tilgang
+	 * til antallet ved å bruke metoden getAntallKort(). Metoden er først og
+	 * fremst ment å brukes i testklasser. Om man trenger kortene utenfor,
+	 * anbefales metoden getAlleKort().
 	 * 
 	 * @return tabell av kort.
 	 */
 	public Kort[] getSamling() {
-		
+
 		return samling;
-		
+
 	}
-	
+
 	/**
 	 * Antall kort i samlingen.
 	 * 
 	 * @return antall kort i samlinga.
 	 */
 	public int getAntalKort() {
-		
+
 		return antall;
 	}
-	
+
 	/**
 	 * Sjekker om samlinga er tom.
 	 * 
 	 * @return true om samlinga er tom, false ellers.
 	 */
 	public boolean erTom() {
-		
-if(antall == 0) {
-	return true;
-}
-else return false;
+
+		if (antall == 0) {
+			return true;
+		} else
+			return false;
 	}
 
 	/**
 	 * Legg et kort til samlinga.
 	 * 
-	 * @param kort
-	 *            er kortet som skal leggast til.
+	 * @param kort er kortet som skal leggast til.
 	 */
 	public void leggTil(Kort kort) {
-while (antall <= MAKS_KORT) {		
-samling[antall] = kort;
-antall++;
-}	
+		if (antall <= MAKS_KORT) {
+			samling[antall] = kort;
+			antall++;
+
+		}
 	}
-	
+
 	/**
 	 * Legger alle korta (hele kortstokken) til samlinga. Korta vil være sortert
 	 * slik at de normalt må stokkes før bruk.
 	 */
 	public void leggTilAlle() {
-		
 
 		// Husk: bruk Regler.MAKS_KORT_FARGE for å få antall kort per farge
-		
-		
-		
-		
-		
-		for(int i = 0; i <= Regler.MAKS_KORT_FARGE; i++) {
-		
-			for(int j = 0; j < 4; j++) {
-				samling[j+4*i] = new Kort(Kortfarge.Hjerter, i);
-				
+
+		for (Kortfarge f : Kortfarge.values()) {
+			for (int i = 1; i <= Regler.MAKS_KORT_FARGE; i++) {
+				samling[i - 1] = new Kort(f, i);
+				antall++;
 			}
 		}
-		antall = MAKS_KORT;
 
 	}
 
@@ -105,42 +100,60 @@ antall++;
 	 * Fjerner alle korta fra samlinga slik at den blir tom.
 	 */
 	public void fjernAlle() {
-		
 
 		samling = new Kort[MAKS_KORT];
+		antall = 0;
 	}
-	
+
 	/**
 	 * Ser på siste kortet i samlinga.
 	 * 
-	 * @return siste kortet i samlinga, men det blir ikke fjernet. Dersom samalinga er tom, returneres
-	 *         null.
+	 * @return siste kortet i samlinga, men det blir ikke fjernet. Dersom samalinga
+	 *         er tom, returneres null.
 	 */
 	public Kort seSiste() {
-		
+
 		// TODO - START
-		
-		return samling[antall - 1];
+
+		if (antall == 0)
+			return null;
+
+		else
+			return samling[antall - 1];
 
 		// TODO - END
-		
+
 	}
 
 	/**
 	 * Tek ut siste kort fra samlinga.
 	 * 
-	 * @return siste kortet i samlinga. Dersom samalinga er tom, returneres
-	 *         null.
+	 * @return siste kortet i samlinga. Dersom samalinga er tom, returneres null.
 	 */
 	public Kort taSiste() {
-		
+
 		// TODO - START
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+
+		if (antall == 0)
+			return null;
+
+		else {
+			for (int i = MAKS_KORT - 1; i >= 0; i--) {
+				if (samling[i] != null) {
+
+					Kort tmp = samling[i];
+					samling[i] = null;
+					antall--;
+					return tmp;
+				}
+
+			}
+			return null;
+		}
+
 		// TODO - END
 	}
-	
+
 	/**
 	 * Undersøker om et kort finst i samlinga.
 	 * 
@@ -149,32 +162,43 @@ antall++;
 	 * @return true om kortet finst i samlinga, false ellers.
 	 */
 	public boolean har(Kort kort) {
-		
-		// TODO - START
-		
-		throw new UnsupportedOperationException(TODO.method());
-		// return false;
-		// TODO - END
-		
+
+		if (kort == null)
+			return false;
+
+		for (int i = 0; samling.length > i; i++) {
+			if (samling[i] == kort)
+				return true;
+			else
+				;
+		}
+		return false;
+
 	}
 
 	/**
-	 * Fjernar et kort frå samlinga. Dersom kortet ikke finnest i samlinga,
-	 * skjer ingenting med samilingen
+	 * Fjernar et kort frå samlinga. Dersom kortet ikke finnest i samlinga, skjer
+	 * ingenting med samilingen
 	 * 
-	 * @param kort
-	 *            kortet som skal fjernast. Dersom kortet ikke finnes, skjer
-	 *            ingenting.
+	 * @param kort kortet som skal fjernast. Dersom kortet ikke finnes, skjer
+	 *             ingenting.
 	 * @return true om kortet blev fjernet fra samlinga, false ellers.
 	 */
-			 
-	public boolean fjern(Kort kort) {
-		
-		// TODO - START
-		
-		throw new UnsupportedOperationException(TODO.method());
 
-		// TODO - END
+	public boolean fjern(Kort kort) {
+
+		if (kort == null)
+			return false;
+
+		for (int i = 0; i < MAKS_KORT; i++) {
+			if (kort == samling[i]) {
+				samling[i] = null;
+				antall--;
+				return true;
+			}
+
+		}
+		return false;
 	}
 
 	/**
@@ -184,12 +208,17 @@ antall++;
 	 *         som i kortsamlinga.
 	 */
 	public Kort[] getAllekort() {
-		
-		// TODO - START
-		
-return samling;
-		// TODO - END
-	
+
+		Kort[] tmp = new Kort[antall];
+		int j = 0;
+		for (int i = 0; i < samling.length; i++) {
+
+			if (samling[i] != null)
+				tmp[j] = samling[i];
+			j++;
+
+		}
+		return tmp;
 	}
-	
+
 }
